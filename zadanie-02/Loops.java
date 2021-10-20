b0d45dd1aa4952616dc3a8fbd7c59a7e4bb1f126
Grzdaczek <grzegorz.jnsk@gmail.com>
2021-10-21 00:33:22 +0200

    Zadanie 2

---
+++ zadanie-02/Loops.java
@@ -0,0 +1,40 @@
+import java.util.List;
+import java.util.LinkedList;
+
+public class Loops implements GeneralLoops {
+	
+	private List<Integer> lowerLimits = null;
+	private List<Integer> upperLimits = null;
+
+	private void loop(int index, List<Integer> acc, List<List<Integer>> result) {
+		if(index == lowerLimits.size() || index == upperLimits.size()) {
+			result.add(acc);
+			return;
+		}
+		else {
+			for(int i = lowerLimits.get(index); i <= upperLimits.get(index); i++) {
+				List<Integer> newAcc = new LinkedList<Integer>(acc);
+				newAcc.add(i);
+				this.loop(index + 1, newAcc, result);
+			}
+		}
+	}
+
+	@Override
+	public List<List<Integer>> getResult() {
+		List<List<Integer>> result = new LinkedList<List<Integer>>(); 
+		List<Integer> acc = new LinkedList<Integer>();
+		loop(0, acc, result);
+		return result;
+	}
+
+	@Override
+	public void setLowerLimits(List<Integer> limits) {
+		this.lowerLimits = limits;
+	}
+	
+	@Override
+	public void setUpperLimits(List<Integer> limits) {
+		this.upperLimits = limits;
+	}
+}
